cb7e7bbdab2e68fd4b44b1cbb8e0fff760c51d75
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-04-19 11:46:44 +0200

    started login

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,72 @@ import { useState } from "react";
 import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  /*
+  THIS CODE DOES NOT WORK YET 
+  */
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  return <div className="App"></div>;
+  const logIn = async (task) => {
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+
+    setUserName(userName);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!userName) {
+      alert("Please add a task");
+      return;
+    }
+    if (!password) {
+      alert("Please add a pokemon");
+      return;
+    }
+
+    onAdd({ userName, password });
+
+    setUserName("");
+    setPassword("");
+  };
+
+  return (
+    <div className="App">
+      <h2>Log in</h2>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label>user name</label>
+          <input
+            className="formInput"
+            type="text"
+            placeholder="user name"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          ></input>
+        </div>
+        <div className="form-control">
+          <label>password</label>
+          <input
+            className="formInput"
+            type="text"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <input type="submit" value="Save Task" className="myButton" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
