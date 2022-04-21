9c6097a5a2252145e75761e46da6ed716fdd4150
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-04-21 13:42:23 +0200

    changed links to match after deployment

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,4 @@
-export const loginUrl =
-  "http://localhost:8080/dat3_startcode_security_war_exploded/api/login";
+export const loginUrl = "https://test.singah.dk/devops-starter/api/login";
 export const pokemonUrl =
-  "http://localhost:8080/dat3_startcode_security_war_exploded/api/info/pokemon";
-export const swapiUrl =
-  "http://localhost:8080/dat3_startcode_security_war_exploded/api/info/swapi";
+  "https://test.singah.dk/devops-starter/api/info/pokemon";
+export const swapiUrl = "https://test.singah.dk/devops-starter/api/info/swapi";
